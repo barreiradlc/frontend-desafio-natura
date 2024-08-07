daa7ecde4a9463f10c3bb0bdf2b6f6139a3d2086
Augusto Barreira <barreira266@hotmail.com>
2024-08-07 11:33:29 -0300

    Header

---
+++ src/components/shared/products/SearchInput.tsx
@@ -0,0 +1,68 @@
+import {
+  Calculator,
+  Calendar,
+  CreditCard,
+  Settings,
+  Smile,
+  User,
+} from "lucide-react"
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command"
+import { useState } from "react"
+
+export function SearchInput() {
+  const [query, setQuery] = useState("")
+
+  return (
+    <Command className={`w-96 rounded-full border shadow-md ${query ? "flex" : "grid"}`} >
+      <CommandInput
+        placeholder="Oque está procurando hoje?"
+        onValueChange={(e) => setQuery(e)}
+      />
+      <CommandList className={query ? "flex" : "hidden"}>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem>
+            <Calendar className="mr-2 h-4 w-4" />
+            <span>Calendar</span>
+          </CommandItem>
+          <CommandItem>
+            <Smile className="mr-2 h-4 w-4" />
+            <span>Search Emoji</span>
+          </CommandItem>
+          <CommandItem disabled>
+            <Calculator className="mr-2 h-4 w-4" />
+            <span>Calculator</span>
+          </CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Settings">
+          <CommandItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+            <CommandShortcut>⌘P</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <CreditCard className="mr-2 h-4 w-4" />
+            <span>Billing</span>
+            <CommandShortcut>⌘B</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Settings className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+            <CommandShortcut>⌘S</CommandShortcut>
+          </CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  )
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,16 @@
+import logoImage from "@/assets/logo.png"
+import { SearchInput } from "@/components/shared/products/SearchInput"
 import { Button } from "@/components/ui/button"
-import { X } from "lucide-react"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { ChevronDown, ShoppingCart, User, X } from "lucide-react"
+
 
 function Home() {
   return (
@@ -14,6 +25,57 @@ function Home() {
             <X color="#ffffff" />
           </Button>
         </div>
+        {/* Top Bar */}
+
+        {/* Header */}
+        <div className="w-screen bg-white text-black py-4">
+          <div className="container flex flex-row justify-between items-center">
+            <div id="logo" className="w-96">
+              <img src={logoImage} />
+            </div>
+            <div id="categories" className="w-24">
+              {/* TODO, considerar fazer componente */}
+              <DropdownMenu >
+                <DropdownMenuTrigger className="w-fill flex flex-row">Produtos <ChevronDown /> </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>Para quem</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem> todo mundo </DropdownMenuItem>
+                  <DropdownMenuItem> masculina </DropdownMenuItem>
+                  <DropdownMenuItem> feminina </DropdownMenuItem>
+                  <DropdownMenuItem> infantil </DropdownMenuItem>
+                  <DropdownMenuLabel>Nossas marcas</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem> biome </DropdownMenuItem>
+                  <DropdownMenuItem> bothânica </DropdownMenuItem>
+                  <DropdownMenuItem> chronos </DropdownMenuItem>
+                  <DropdownMenuItem>crer Para  Ver </DropdownMenuItem>
+                  <DropdownMenuItem> ekos </DropdownMenuItem>
+                  <DropdownMenuItem> essencial </DropdownMenuItem>
+                  <DropdownMenuItem> kaiak </DropdownMenuItem>
+                  <DropdownMenuItem> lumina </DropdownMenuItem>
+                  <DropdownMenuItem> luna </DropdownMenuItem>
+                  <DropdownMenuItem>mamãe e  Bebê </DropdownMenuItem>
+                  <DropdownMenuItem> tododia </DropdownMenuItem>
+                  <DropdownMenuItem> una </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+            </div>
+            <div id="search" className="w-fit">
+              <SearchInput />
+            </div>
+            <div id="links">
+              <Button variant="link">
+                <ShoppingCart />
+              </Button>
+              <Button variant="link">
+                <User />
+              </Button>
+            </div>
+          </div>
+        </div>
+        {/* Header */}
 
       </div>
 

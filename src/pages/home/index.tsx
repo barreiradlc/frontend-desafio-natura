87dc362a0923e90d3411e95313e3a912010cf3f2
Augusto Barreira <barreira266@hotmail.com>
2024-08-07 10:01:12 -0300

    Top Bar

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,24 @@
+import { Button } from "@/components/ui/button"
+import { X } from "lucide-react"
+
 function Home() {
-  return <h1>Home page</h1>
+  return (
+    <div>
+      {/* Top Bar */}
+      <div className="w-screen bg-slate-600 text-white">
+        <div className="container flex flex-row justify-between items-center">
+          <div className="w-full flex flex-row justify-center">
+            <span className="">Aproveite as nossas oportunidades!!!</span>
+          </div>
+          <Button variant="link">
+            <X color="#ffffff" />
+          </Button>
+        </div>
+
+      </div>
+
+    </div>
+  )
 }
 
 export { Home }

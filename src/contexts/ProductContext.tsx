f015be313bcdb179d65accb3e81355e2a5d7ac4d
Augusto Barreira <barreira266@hotmail.com>
2024-08-08 14:29:38 -0300

    Adição de valores no contexto

--- src/contexts/ProductContext.tsx
+++ src/contexts/ProductContext.tsx
@@ -1,5 +1,5 @@
 import { api } from "@/lib/http/api";
-import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { createContext, ReactNode, useCallback, useContext, useEffect, useState } from "react";
 
 interface IProduct {
   id: string;
@@ -8,8 +8,10 @@ interface IProduct {
 }
 
 type ProductContextType = {
+  reachedTheEnd: boolean;
   products: IProduct[];
   fetchProducts: (query?: string) => Promise<void>
+  fetchMoreProducts: () => Promise<void>
 }
 
 const ProductContext = createContext({} as ProductContextType)
@@ -19,21 +21,45 @@ type ProductContextProviderProps = {
 }
 
 function ProductContextProvider({ children }: ProductContextProviderProps) {
-
+  // TODO, melhorar este nome
+  const [reachedTheEnd, setReachedTheEnd] = useState(false)
   const [products, setProducts] = useState<IProduct[]>([])
+  const [searchParams, setSearchParams] = useState({
+    take: 4,
+    page: 1,
+    query: ''
+  })
+
+  const fetchProducts = useCallback(async () => {
+    const { query, take, page } = searchParams
+    const skip = (page * take) - take
+    const { data } = await api.get(`/products?query=${query}&take=${take}&skip=${skip}`)
+
+    setProducts([
+      ...products,
+      ...data
+    ])
+    setSearchParams({
+      ...searchParams,
+      page: page + 1,
+    })
+
+    console.log(data.length, reachedTheEnd)
+
+    if (data.length === 0) setReachedTheEnd(true);
+  }, [products, searchParams])
 
   useEffect(() => {
     fetchProducts()
   }, [])
 
-  async function fetchProducts(query = '') {
-    const { data } = await api.get(`/products?query=${query}`)
-
-    setProducts(data)
+  async function fetchMoreProducts() {
+    console.log({ searchParams })
+    fetchProducts()
   }
 
   return (
-    <ProductContext.Provider value={{ products, fetchProducts }}>
+    <ProductContext.Provider value={{ products, fetchProducts, fetchMoreProducts, reachedTheEnd }}>
       {children}
     </ProductContext.Provider>
   )
